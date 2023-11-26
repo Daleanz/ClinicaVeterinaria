d7bbe4b03bef70603da12fc047a1e2c8e51919a4
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-11-26 15:42:06 -0300

    Update Guardia.cs

--- Trabajadores/Guardia.cs
+++ Trabajadores/Guardia.cs
@@ -1,40 +1,16 @@
 using System;
 using Generadores;
-using Interfaces;
-using Inventario;
 
 namespace Trabajadores{
-    class Cajero : Persona, ICajero{
+    class Guardia : Persona{
         private string tiempoDeServicio;
-        private string correoElectronicoCajero;
-        private string direccionCajero;
+        private string correoElectronicoGuardia;
+        private string direccionGuardia;
 
-        public Cajero(string nombrePersona, string apellidoPersona, string rutPersona, string telefonoPersona, string tiempoDeServicio, string correoElectronicoCajero, string direccionCajero) : base(nombrePersona, apellidoPersona, rutPersona, telefonoPersona){
+        public Guardia(string nombrePersona, string apellidoPersona, string rutPersona, string telefonoPersona, string tiempoDeServicio, string correoElectronicoGuardia, string direccionGuardia) : base(nombrePersona, apellidoPersona, rutPersona, telefonoPersona){
             this.tiempoDeServicio = tiempoDeServicio;
-            this.correoElectronicoCajero = correoElectronicoCajero;
-            this.direccionCajero = direccionCajero;
-        }
-
-        public void VenderProducto(Bodega bodega, Producto producto, int cantidad){
-            if(bodega.ObtenerCapacidadRestante() < cantidad){
-                System.Console.WriteLine($"No existen suficientes productos de: {producto.NombreProducto}");
-            }else{
-                for(int i = 0; i < cantidad; i ++){
-                    bodega.EliminarProductoInventario(producto);
-                }
-            }
-            // Falta generar la boleta.
-            System.Console.WriteLine($"Si se puede vender {cantidad} unidades de {producto.NombreProducto}");
-        }
-
-        public string CorreoElectronicoCajero{
-            get{return correoElectronicoCajero;}
-            set{this.correoElectronicoCajero = value;}
-        }
-
-        public string DireccionCajero{
-            get{return direccionCajero;}
-            set{this.direccionCajero = value;}
+            this.correoElectronicoGuardia = correoElectronicoGuardia;
+            this.direccionGuardia = direccionGuardia;
         }
 
         public string TiempoDeServicio{
@@ -42,15 +18,25 @@ namespace Trabajadores{
             set{this.tiempoDeServicio = value;}
         }
 
+        public string CorreoElectronicoGuardia{
+            get{return correoElectronicoGuardia;}
+            set{this.correoElectronicoGuardia = value;}
+        }
+
+        public string DireccionGuardia{
+            get{return direccionGuardia;}
+            set{this.direccionGuardia = value;}
+        }
+
         public override void MostrarInformacion(){
-            System.Console.WriteLine("Informacion Cajero");
+            System.Console.WriteLine("Informacion Guardia");
             System.Console.WriteLine($"Nombre: {base.NombrePersona}\n"+
                     $"Apellido: {base.ApellidoPersona}\n"+
                     $"Rut: {base.RutPersona}\n"+
                     $"Telefono: {base.TelefonoPersona}\n"+
-                    $"Correo: {CorreoElectronicoCajero}\n"+
-                    $"Direccion: {DireccionCajero}\n"+
-                    $"Tiempo de Servicio: {TiempoDeServicio}");
+                    $"Correo: {CorreoElectronicoGuardia}\n"+
+                    $"Direccion: {DireccionGuardia}\n"+
+                    $"Tiempo de Sevicio: {TiempoDeServicio}");
         }
     }
 }
