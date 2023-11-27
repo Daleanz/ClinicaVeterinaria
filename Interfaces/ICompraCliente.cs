d346baf4429dfb09229f9c8c0f60dfbe521346a1
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-11-26 23:19:21 -0300

    Update ICompraCliente.cs

--- Interfaces/ICompraCliente.cs
+++ Interfaces/ICompraCliente.cs
@@ -1,10 +1,11 @@
 using System;
 using Generadores;
+using Productos;
 
 namespace Interfaces{
     interface ICompraCliente{
         void ComprarProducto(Producto producto, int cantidad);
         void DevolverProducto(Producto producto, int cantidad);
-        int ContarTipoProducto<T>(List<Object> lista);
+        int ContarTipoProducto<T>(List<Producto> lista);
     }
 }
