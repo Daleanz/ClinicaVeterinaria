efbe9e53689398656f6b5c1ec756198f295ba1ee
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-11-26 23:22:09 -0300

    Update Radiografia.cs

--- SaludAnimal/Radiografia.cs
+++ SaludAnimal/Radiografia.cs
@@ -4,12 +4,18 @@ namespace SaludAnimal{
     class Radiografia{
         private string tipoRadiografia;
         private double costoRadiografia;
+        private int cantidadRadiografia;
 
-        public Radiografia(string tipoRadiografia, double costoRadiografia){
+        public Radiografia(string tipoRadiografia, double costoRadiografia, int cantidadRadiografia){
             this.tipoRadiografia = tipoRadiografia;
             this.costoRadiografia = costoRadiografia;
+            this.cantidadRadiografia = cantidadRadiografia;
         }
 
+        public int CantidadRadiografia{
+            get{return cantidadRadiografia;}
+            set{this.cantidadRadiografia = value;}
+        }
         public string TipoRadiografia{
             get{return tipoRadiografia;}
             set{this.tipoRadiografia = value;}
@@ -23,7 +29,9 @@ namespace SaludAnimal{
         public void MostrarInformacionRadiografia(){
             System.Console.WriteLine("Informacion Radiografia");
             System.Console.WriteLine($"Tipo: {TipoRadiografia}\n"+
-                    $"Costo: {CostoRadiografia}");
+                    $"Costo: {CostoRadiografia}\n"+
+                    $"Cantidad: {CantidadRadiografia}\n"+
+                    $"Total: {CantidadRadiografia * CostoRadiografia}");
         }
     }
 }
