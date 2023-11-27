82c16e56e921eb31b85d256d3b807c5b6cd9c3cd
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-11-26 23:13:27 -0300

    Update ClienteNormal.cs

--- Clientes/ClienteNormal.cs
+++ Clientes/ClienteNormal.cs
@@ -1,9 +1,113 @@
 using System;
 using Generadores;
+using Interfaces;
+using SaludAnimal;
+using Productos;
 
 namespace Clientes{
-    class ClienteNormal : Persona{
+    class ClienteNormal : Persona,ICompraCliente, ICliente{
+        private List<Tratamiento> tratamientos;
+        private List<Cirugia> cirugias;
+        private List<Radiografia> radiografias;
+        private List<Producto> compras;
+
         public ClienteNormal(string nombrePersona, string apellidoPersona, string rutPersona, string telefonoPersona) : base(nombrePersona, apellidoPersona, rutPersona, telefonoPersona){
+            this.tratamientos = new List<Tratamiento>();
+            this.cirugias = new List<Cirugia>();
+            this.radiografias = new List<Radiografia>();
+            this.compras = new List<Producto>();
+        }
+
+        public void ComprarProducto(Producto producto, int cantidad){
+            compras.Add(producto);
+        }
+
+        public void DevolverProducto(Producto producto, int cantidad){
+            if(compras.Count == 0){
+                System.Console.WriteLine("No puede devolver nada, porque no tiene ningun producto.");
+            } 
+            if(compras.Count != 0){
+                int tipoComidaPerro = ContarTipoProducto<ComidaPerro>(compras);
+                int tipoComidaGato = ContarTipoProducto<ComidaGato>(compras);
+                int tipoJuguete = ContarTipoProducto<Juguete>(compras);
+
+                foreach(var pro in compras){
+                    if(producto == pro){
+                        for(int i = 0; i < cantidad; i ++){
+                            compras.Remove(pro);
+                        }
+                    } 
+                }
+                System.Console.WriteLine("Se han devuelto correctamente los productos.");
+            }
+        }
+
+        public int ContarTipoProducto<T>(List<Producto> lista){
+            return lista.Count(item => item is T);
+        }
+
+        public List<Producto> ObtenerCompras{
+            get{return compras;}
+        }
+
+        public List<Tratamiento> ObtenerTratamientos{
+            get{return tratamientos;}
+        }
+
+        public List<Radiografia> ObtenerRadiografias{
+            get{return radiografias;}
+        }
+
+        public List<Cirugia> ObtenerCirugias{
+            get{return cirugias;}
+        }
+
+        public void AgregarTratamiento(Tratamiento tratamiento){
+            tratamientos.Add(tratamiento);
+        }
+
+        public void AgregarCirugia(Cirugia cirugia){
+            cirugias.Add(cirugia);
+        }
+
+        public void AgregarRadiografia(Radiografia radiografia){
+            radiografias.Add(radiografia);
+        }
+
+        public void EliminarTratamiento(Tratamiento tratamiento){
+            int contador = 0;
+
+            if(tratamientos.Contains(tratamiento)){
+                contador = contador + 1;
+            }
+
+            if(contador > 1){
+                tratamientos.Remove(tratamiento);
+            }
+        }
+
+        public void EliminarCirugia(Cirugia cirugia){
+            int contador = 0;
+
+            if(cirugias.Contains(cirugia)){
+                contador = contador + 1;
+            }
+
+            if(contador > 1){
+                cirugias.Remove(cirugia);
+            }
+        }
+
+        public void EliminarRadiografia(Radiografia radiografia){
+            int contador = 0;
+
+            if(radiografias.Contains(radiografia)){
+                contador = contador + 1;
+            }
+
+            if(contador > 1){
+                radiografias.Remove(radiografia);
+            }
         }
 
         public override void MostrarInformacion(){
@@ -13,5 +117,6 @@ namespace Clientes{
                     $"Rut: {base.RutPersona}\n"+
                     $"Telefono: {base.TelefonoPersona}\n");
         }
+
     }
 }
