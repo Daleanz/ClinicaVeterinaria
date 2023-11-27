c72c4a748bcecb4d80e8fd5af33735b5b97389f1
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-11-26 23:18:37 -0300

    Update ICajero.cs

--- Interfaces/ICajero.cs
+++ Interfaces/ICajero.cs
@@ -1,9 +1,11 @@
 using System;
 using Generadores;
 using Inventario;
+using Clinica;
 
 namespace Interfaces{
     interface ICajero{
         void VenderProducto(Bodega bodega, Producto producto, int cantidad);
+        void GenerarBoleta(ICliente cliente, ClinicaVET clinica);
     }
 }
