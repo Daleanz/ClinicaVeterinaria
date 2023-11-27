9583a1b8d953e358a282eb351fe857e285267520
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-11-26 23:21:04 -0300

    Update ComidaPerro.cs

--- Productos/ComidaPerro.cs
+++ Productos/ComidaPerro.cs
@@ -5,7 +5,7 @@ namespace Productos{
     class ComidaPerro : Producto{
         private string tipoComida;
 
-        public ComidaPerro(string nombreProducto, double precioProducto, string tipoComida) : base(nombreProducto, precioProducto){
+        public ComidaPerro(string nombreProducto, double precioProducto, int cantidadProducto, string tipoComida) : base(nombreProducto, precioProducto, cantidadProducto){
             this.tipoComida = tipoComida;
         }
 
@@ -17,7 +17,8 @@ namespace Productos{
         public override void MostrarInformacionProducto(){
             System.Console.WriteLine($"Informacion del Producto: {base.NombreProducto}");
             System.Console.WriteLine($"Tipo: {TipoComida}\n"+
-                    $"Precio: ${base.PrecioProducto}");
+                    $"Precio: ${base.PrecioProducto}\n"+
+                    $"Cantidad: {base.CantidadProducto}");
         }
     }
 }
