0e5e8edd27b4b414003e6b4800f27b95d960e04c
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-11-26 23:23:50 -0300

    Update Veterinario.cs

--- Trabajadores/Veterinario.cs
+++ Trabajadores/Veterinario.cs
@@ -1,5 +1,6 @@
 using System;
 using Generadores;
+using Interfaces;
 
 namespace Trabajadores{
     class Veterinario : Persona{
@@ -7,13 +8,35 @@ namespace Trabajadores{
         private string correoElectronicoVeterinario;
         private string direccionVeterinario;
         private string tiempoDeServicio;
+        private List<ICliente> clientes;
 
         public Veterinario(string nombrePersona, string apellidoPersona, string rutPersona, string telefonoPersona, string tipoDeVeterinario, string correoElectronicoVeterinario, string direccionVeterinario, string tiempoDeServicio) : base(nombrePersona, apellidoPersona, rutPersona, telefonoPersona){
             this.tipoDeVeterinario = tipoDeVeterinario;
             this.correoElectronicoVeterinario = correoElectronicoVeterinario;
             this.direccionVeterinario = direccionVeterinario;
             this.tiempoDeServicio = tiempoDeServicio;
+            this.clientes = new List<ICliente>();
+        }
 
+        public void AgregarCliente(ICliente cliente){
+            if(clientes.Count == 0){
+                clientes.Add(cliente);
+                System.Console.WriteLine($"Se asigno correctamente el Cliente al Veterinario: {base.NombrePersona}");
+            }
+            if(clientes.Contains(cliente)){
+                System.Console.WriteLine($"No se puede asignar el mismo cliente.");
+                cliente.MostrarInformacion();
+            }
+        }
+
+        public void EliminarCliente(ICliente cliente){
+            if(clientes.Count == 0){
+                System.Console.WriteLine($"No tiene ningun paciente asignado el Veterniario: {base.NombrePersona}");
+            }
+            if(clientes.Contains(cliente)){
+                clientes.Remove(cliente);
+                System.Console.WriteLine($"Se ha eliminado correctamente el paciente de: {base.NombrePersona}");
+            }
         }
 
         public string TiempoDeServicio{
